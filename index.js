3c150f0cfb3f7d77d17bc29517b4bb48472f13dc
Tilde <tilde@thuryism.net>
2024-09-18 10:41:54 -0700

    Serve files from static directory

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const path = require("path");
+const serveStatic = require("serve-static");
 const app = express();
+
+app.use(serveStatic(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
   res.send("Hello World");
