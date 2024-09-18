5b4b0c4b030856989dcb18f7db8014b7fb4b4543
Tilde <tilde@thuryism.net>
2024-09-18 15:54:08 -0700

    add LaunchDarkly conditional in routing

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 const express = require("express");
 const path = require("path");
 const serveStatic = require("serve-static");
+
 const app = express();
+
+const ld = require("@launchdarkly/node-server-sdk");
+require("dotenv").config();
+
+const sdkKey = process.env.LAUNCHDARKLY_SDK_KEY;
+const ldClient = ld.init(sdkKey);
 
 app.use(serveStatic(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.get("/", async function (req, res) {
+  const context = {
+    kind: "user",
+    key: "user-key-123abcde",
+    email: "biz@face.dev",
+  };
+  const showStudentVersion = await ldClient.variation(
+    "show-student-version",
+    context,
+    false
+  );
+  let fileName;
+  if (showStudentVersion) {
+    fileName = "student.html";
+  } else {
+    fileName = "enterprise.html";
+  }
+  res.redirect(fileName);
 });
 
 const port = 3000;
-app.listen(port, function (err) {
+const server = app.listen(port, function (err) {
   if (err) console.log("Error in server setup");
   console.log(`Server listening on http://localhost:${port}`);
+});
+
+process.on("SIGTERM", () => {
+  debug("SIGTERM signal received: closing HTTP server");
+  ld.close();
+  server.close(() => {
+    debug("HTTP server closed");
+    ldClient.close();
+  });
 });
